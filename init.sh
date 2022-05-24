287a68e3c8b2c4dbac9753e974b52d53b1ab7edf
Veselov Anton <veselov95.anton@gmail.com>
2022-05-24 17:29:45 +0300

    1.9

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
